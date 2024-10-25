1aeae1a366fd0cad701d3c967594ab33de5bbd8e
jkhan13 <jkhan3@ocdsb.ca>
2024-10-25 17:02:14 -0400

    carousel done!!

--- WebpageProject/script.js
+++ WebpageProject/script.js
@@ -1,8 +1,21 @@
-function showTime() {
-  document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
-  showTime();
-}, 1000);
+// function showTime() {
+//   document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+// }
+// showTime();
+// setInterval(function () {
+//   showTime();
+// }, 1000);
 
+var imgList = document.getElementById('imgList');
+var scrollRight = document.getElementById('scroll-right');
+var scrollLeft = document.getElementById('scroll-left');
+
+// When a user clicks on the right arrow, the ul will scroll 750px to the right
+scrollRight.addEventListener('click', (event) => {
+  imgList.scrollBy(750, 0);
+});
+
+// When a user clicks on the left arrow, the ul will scroll 750px to the left
+scrollLeft.addEventListener('click', (event) => {
+  imgList.scrollBy(-750, 0);
+});
